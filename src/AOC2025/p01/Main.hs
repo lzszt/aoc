8ce392a6e7e5eb4f9317e53d4c72c62ff1a0ab28
Felix Leitz <felix.leitz92@gmail.com>
2025-12-01 16:55:35 +0100

    25d01: Part 1

---
+++ src/AOC2025/p01/Main.hs
@@ -0,0 +1,46 @@
+module Main where
+
+import System.Environment
+
+data Rotation
+  = RotateLeft Int
+  | RotateRight Int
+
+type Input = [Rotation]
+
+parseInput :: FilePath -> IO Input
+parseInput path = map readRotation . lines <$> readFile path
+  where
+    readRotation = \case
+      'L' : distance -> RotateLeft $ read distance
+      'R' : distance -> RotateRight $ read distance
+      x -> error $ "Unknown rotation: " <> x
+
+initialDialPosition :: Int
+initialDialPosition = 50
+
+part1 :: Input -> Int
+part1 =
+  length
+    . filter (== 0)
+    . snd
+    . foldl'
+      ( \(currentPosition, previousPositions) rot ->
+          let newPosition = applyRotation currentPosition rot
+           in (newPosition, newPosition : previousPositions)
+      )
+      (initialDialPosition, [])
+  where
+    applyRotation pos = \case
+      RotateLeft dist -> (pos - dist) `mod` 100
+      RotateRight dist -> (pos + dist) `mod` 100
+
+part2 :: Input -> Int
+part2 = const 0
+
+main :: IO ()
+main = do
+  [args] <- getArgs
+  input <- parseInput args
+  print $ part1 input
+  print $ part2 input
